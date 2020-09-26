197a0689e8f12ea00a55297599bb0a0db60d3ba0
rakeshdr543 <rakeshdr543@gmail.com>
2020-09-27 00:35:20 +0530

    Updates

---
+++ precache-manifest.0ed805061567e3003fa06b9b92267683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f06ced4381489488575b4d9d88f58d",
+    "url": "/newsapp/index.html"
+  },
+  {
+    "revision": "26d49ac1cd3599c7576a",
+    "url": "/newsapp/static/css/main.1327d87a.chunk.css"
+  },
+  {
+    "revision": "f7133bab6b53b12c6351",
+    "url": "/newsapp/static/js/2.71548172.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/newsapp/static/js/2.71548172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d49ac1cd3599c7576a",
+    "url": "/newsapp/static/js/main.dea34a3c.chunk.js"
+  },
+  {
+    "revision": "14c2ef09e13035347734",
+    "url": "/newsapp/static/js/runtime-main.eff0c8ba.js"
+  }
+]);
